b2674fb98ede87d438f9fed76b8f4852aa523ff2
rkprasad1101-cyber <rkprasad1101@gmail.com>
2026-07-21 23:27:56 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,21 @@
+# Product Configuration
+
+COUNTRY = "IN"
+
+STORE_NAME = "Apple Saket"
+
+PRODUCT_NAME = "iPhone 17"
+
+STORAGE = "256GB"
+
+COLORS = [
+    "Black",
+    "White",
+    "Mist Blue",
+    "Sage",
+    "Lavender"
+]
+
+CHECK_INTERVAL = 600  # seconds
+
+TELEGRAM_PARSE_MODE = "HTML"
